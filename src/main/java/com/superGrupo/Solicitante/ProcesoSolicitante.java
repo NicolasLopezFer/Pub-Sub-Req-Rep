d9b9fa61b0e5b07f846121b5e0d68c30ab790628
Nicolas Lopez <nlopez97f@gmail.com>
2021-05-24 21:05:03 -0700

    fix

--- src/main/java/com/superGrupo/Solicitante/ProcesoSolicitante.java
+++ src/main/java/com/superGrupo/Solicitante/ProcesoSolicitante.java
@@ -12,18 +12,26 @@ import java.util.Scanner;
 public class ProcesoSolicitante {
     public static void main( String[] args ) throws Exception 
     {
+        System.setProperty("java.rmi.server.hostname", "25.80.215.15");
 
         ZMQ.Context context = ZMQ.context(1);
+        ZMQ.Context context2 = ZMQ.context(1);
 
         ZMQ.Socket requester = context.socket(ZMQ.REQ);
-        requester.connect("tcp://localhost:9999");
-        LocalDateTime now1 = LocalDateTime.now();
+        ZMQ.Socket requester2 = context2.socket(ZMQ.REQ);
 
+        requester.connect("tcp://25.16.253.86:10005");
+        requester2.connect("tcp://25.12.85.91:10006");
+
+        LocalDateTime now1 = LocalDateTime.now();
+        boolean gestor1 = true;
+        boolean gestor2 = true;
+        
         try{
-            File myObj = new File("provider/3PS_250.txt");
+            File myObj = new File("provider/ps.txt");
             Scanner myReader = new Scanner(myObj);
 
-            requester.setReceiveTimeOut(400);
+            requester.setReceiveTimeOut(500);
 
             String sede="Sede1";
             while(myReader.hasNextLine()){
@@ -31,23 +39,60 @@ public class ProcesoSolicitante {
                 data = data + ","+ now1;
                 byte[] reply;
 
+                if(gestor1 == true && gestor2 == false){
+                    gestor1 = false;
+                    gestor2 = true;
+                } else if(gestor1 == false && gestor2 == true){
+                    gestor1 = true;
+                    gestor2 = false;
+                }
+
+                
+
                 do{
-                    try{
-                        requester.send(data.getBytes(), 0);
+                    reply = null;
+                    if(gestor1){
+                        try{
+                            requester.send(data.getBytes(), 0);
+                        }
+                        catch(Exception e){
+                            System.out.println("Gestor sede 1 no activo");
+                        }
+                        reply = requester.recv(0);
+                        if(reply!=null){
+                            gestor2 = false;
+                            gestor1 = true;
+                            String respuesta = new String(reply, StandardCharsets.UTF_8);
+                            System.out.println("RESPUESTA: " + respuesta);
+                        }
+                        else {
+                            gestor2 = true;
+                            gestor1 = false;
+                            sede="Sede2";
+                        }
                     }
-                    catch(Exception e){
-                        System.out.println("No hay gestor");
+
+                    if(gestor2){
+                        try{
+                            requester2.send(data.getBytes(), 0);
+                        }
+                        catch(Exception e){
+                            System.out.println("Gestor sede 2 no activo");
+                        }
+                        reply = requester2.recv(0);
+                        if(reply!=null){
+                            gestor1 = false;
+                            gestor2 = true;
+                            String respuesta = new String(reply, StandardCharsets.UTF_8);
+                            System.out.println("RESPUESTA: " + respuesta);
+                        }
+                        else {
+                            gestor1 = true;
+                            gestor2 = false;
+                            sede="Sede2";
+                        }
                     }
                 
-                    reply = requester.recv(0);
-                    if(reply!=null){
-                        String respuesta = new String(reply, StandardCharsets.UTF_8);
-                        System.out.println("RESPUESTA: " + respuesta);
-                    }
-                    else {
-                        sede="Sede2";
-                        System.out.println("Intenta con otro gestor");
-                    }
                 }while(reply==null);  
             }
             myReader.close();
